3587bf93c13c487ae4173d19b0fb31af479331de
tjdreams <1712853213@qq.com>
2018-04-14 22:54:07 +0800

    优化详细页面图片展示

--- application/admin/config.php
+++ application/admin/config.php
@@ -177,6 +177,7 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str'      => [
         '__UPLOAD__' => '/public/uploads',
+        '__UPLOADS__' => '/public',
         '__STATIC__' => '/public/static',
         '__IMAGES__' => '/public/static/images',
         '__JS__'     => '/public/static/js',
